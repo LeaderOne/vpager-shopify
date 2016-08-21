73d497484fb6579361630f525c64d3e66dc722fa
LeaderOne <markelba1@gmail.com>
2016-08-21 18:08:59 -0500

    Separate front-end from back-end code

--- vpager-db/src/main/scripts/rundocker.sh
+++ vpager-db/src/main/scripts/rundocker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --name vpager --net=vpager-net -e POSTGRES_PASSWORD=N0t@Number -p 5432:5432 -d leaderone/fenrircyndb
+docker run --name vpager -h vpager --net=vpager-net -e POSTGRES_PASSWORD=N0t@Number -p 5432:5432 -d leaderone/fenrircyndb
