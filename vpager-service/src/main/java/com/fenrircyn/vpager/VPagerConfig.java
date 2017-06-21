b0532c2ae0732853e01c1ebb921659c4b34079c3
LeaderOne <markelba1@gmail.com>
2017-06-20 22:09:12 -0500

    Finally, a way to redirect the user when authentication is done.

--- vpager-service/src/main/java/com/fenrircyn/vpager/VPagerConfig.java
+++ vpager-service/src/main/java/com/fenrircyn/vpager/VPagerConfig.java
@@ -6,30 +6,25 @@ import com.fenrircyn.vpager.filters.ShopifyValidator;
 import org.apache.catalina.filters.RequestDumperFilter;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 import javax.servlet.Filter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Configuration
 @EnableOAuth2Client
@@ -44,10 +39,12 @@ public class VPagerConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.antMatcher("/**").authorizeRequests().antMatchers("/install**", "/shopify**", "/shopify/**").permitAll().anyRequest()
+        http.antMatcher("/**")
+                .authorizeRequests().antMatchers("/install**", "/shopify**", "/shopify/**")
+                    .permitAll().anyRequest()
                 .authenticated().and().exceptionHandling()
-                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/install")).and().logout()
-                .logoutSuccessUrl("/").permitAll()
+                    .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/install"))
+                .and().logout().logoutSuccessUrl("/logout").permitAll()
                 .and().csrf().disable()
                 .addFilterBefore(ssoFilter(shopifyRestTemplate()), BasicAuthenticationFilter.class);
     }
@@ -66,6 +63,7 @@ public class VPagerConfig extends WebSecurityConfigurerAdapter {
         return new OAuth2RestTemplate(shopify(), oauth2ClientContext);
     }
 
+
     private Filter ssoFilter(OAuth2RestTemplate shopifyRestTemplate) {
         OAuth2ClientAuthenticationProcessingFilter vpagerFilter = new OAuth2ClientAuthenticationProcessingFilter(
                 "/install");
@@ -75,6 +73,7 @@ public class VPagerConfig extends WebSecurityConfigurerAdapter {
         UserInfoTokenServices tokenServices = new UserInfoTokenServices(shopifyResource().getUserInfoUri(), shopify().getClientId());
         tokenServices.setRestTemplate(shopifyRestTemplate);
         vpagerFilter.setTokenServices(tokenServices);
+        vpagerFilter.setAuthenticationSuccessHandler(new SimpleUrlAuthenticationSuccessHandler("/somewhere"));
 
         return vpagerFilter;
     }
