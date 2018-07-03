a47728845ccbe893a2a6b000f31f593691054bb2
LeaderOne <markelba1@gmail.com>
2018-07-03 12:44:46 -0500

    Catching up code from local

--- vpager-service/src/main/java/com/fenrircyn/vpager/security/filters/ShopifyAPIValidationFilter.java
+++ vpager-service/src/main/java/com/fenrircyn/vpager/security/filters/ShopifyAPIValidationFilter.java
@@ -3,7 +3,9 @@ package com.fenrircyn.vpager.security.filters;
 import com.fenrircyn.vpager.security.providers.ShopifyApiAuthenticationProvider;
 import com.fenrircyn.vpager.security.token.ShopifyAuthenticationToken;
 import com.fenrircyn.vpager.security.token.ShopifyProxyAuthenticationToken;
+import com.google.common.base.Joiner;
 import com.google.common.collect.Sets;
+import org.apache.commons.collections4.MapUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,6 +14,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.UriTemplate;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
@@ -19,10 +22,16 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by markelba on 7/15/17.
@@ -57,7 +66,7 @@ public class ShopifyAPIValidationFilter extends AbstractAuthenticationProcessing
         Authentication isValid = null;
         Map<String, String[]> parameterMap = request.getParameterMap();
 
-        logger.debug("Filtering inbound request for Shopify API.  Params: {}", Arrays.toString(parameterMap.entrySet().toArray()));
+        logger.debug("Filtering inbound request to {} for Shopify API.  Params: {}", request.getRequestURI(), prettyPrintParams(parameterMap));
 
         if(parameterMap.keySet().stream().anyMatch(Sets.newHashSet("customer_id", "hash", "email", "path_prefix",
                 "shop", "timestamp", "signature")::contains)) {
@@ -67,6 +76,21 @@ public class ShopifyAPIValidationFilter extends AbstractAuthenticationProcessing
         }
 
         return isValid;
+    }
+
+    private String prettyPrintParams(Map<String, String[]> parameterMap) {
+        StringBuilder sb = new StringBuilder();
+
+        for(Map.Entry<String,String[]> mapEntry : parameterMap.entrySet()) {
+            sb.append(mapEntry.getKey()).append("=");
+            String delim = "";
+            for(String s: mapEntry.getValue()) {
+                sb.append(s).append(delim);
+                delim = ",";
+            }
+        }
+
+        return sb.toString();
     }
 
     public Authentication attemptAuthenticationOfWebhook(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
@@ -84,8 +108,9 @@ public class ShopifyAPIValidationFilter extends AbstractAuthenticationProcessing
     private Authentication attemptAuthenticationOfProxyRequest(HttpServletRequest request, HttpServletResponse response) {
         ShopifyProxyAuthenticationToken shopifyProxyAuthenticationToken = null;
 
+        long customerId = getCustomerId(request.getRequestURI());
+
         try {
-            long customerId = Long.parseLong(request.getParameter("customer_id"));
             String hash = request.getParameter("hash");
             String shopEmail = request.getParameter("email");
             String pathPrefix = request.getParameter("path_prefix"); //Shopify included automatically
@@ -107,12 +132,22 @@ public class ShopifyAPIValidationFilter extends AbstractAuthenticationProcessing
 
             authenticationProvider.authenticate(shopifyProxyAuthenticationToken);
         } catch (NumberFormatException e) {
-            logger.error("Either customer or timestamp was not a valid number, customer: " +
-                    request.getParameter("customer_id") + ", timestamp: " +
+            logger.error("Either customer or timestamp was not a valid number in URL {}, customer: {}, timestamp {}",
+                    request.getRequestURI(),
+                    customerId,
                     request.getParameter("timestamp"));
         }
 
         return shopifyProxyAuthenticationToken;
+    }
+
+    private long getCustomerId(String requestURI) {
+        Pattern merchantIdPattern = Pattern.compile("/merchant/([0-9]+)/");
+        Matcher matcher = merchantIdPattern.matcher(requestURI);
+
+        String merchantString = matcher.group();
+
+        return Long.getLong(merchantString);
     }
 
     @Override
